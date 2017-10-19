c6bee337d67edfd59678ea70226a74c9ee2916b3
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-10-19 15:13:03 -0400

    fixed footer issue

--- public/ganache.js
+++ public/ganache.js
@@ -2,20 +2,37 @@ $(document).ready(function() {
 
 
 
+    // Sets black border around Shop link in navbar
+    document.getElementById('shopBtn').setAttribute('style', 'outline: 4px solid black; outline-offset:-4px;');
+
+
+
+    
+    if (window.screen.width < 1100) {
+        document.getElementById('desktopNav').setAttribute('style', 'display:none;');
+        document.getElementById('adminLogin').setAttribute('style', 'display:none;');
+    }
+      //if statement hides mobile navbar if device is more than 1100 pixels wide
+      // if (window.screen.width >= 1100) {
+      //   document.getElementById('mobileNav').setAttribute('style', 'display:none;');
+      // }
+
+
     // On page load, calls getContent function and populates attributes for the navbar image
     getContent('navbarPage').then(function(pageContent) {
         document.getElementById('logo-main').setAttribute('src', pageContent.image1.url);
         document.getElementById('logo-main').setAttribute('alt', pageContent.image1.description);
-        
-    })
+    });
     
+
 
     // On page load, calls getContent function and populates attributes for footer content
     getContent('footerPage').then(function(pageContent) {
-        document.getElementById('footerParagraph').setAttribute('innerText', pageContent.footerParagraph);
+        console.log(pageContent.footerParagraph);
+        document.getElementById('footerParagraph').innerText = pageContent.footerParagraph;
         document.getElementById('footerInstagramLink').setAttribute('href', pageContent.footerSocialMedia.instagram);
         document.getElementById('footerFacebookLink').setAttribute('href', pageContent.footerSocialMedia.facebook);
-    })
+    });
 
 
 
