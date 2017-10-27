a1733d431c26e614b0c55695a3a9c757805bdf9f
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-10-27 14:44:42 -0400

    warning modal pops up if no cold pack in cart AND been at least half hour since last popup

--- public/ganache.js
+++ public/ganache.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
 
 
-    let coldPackWarning = false;
+    var coldPackSuggested = window.localStorage.getItem('coldPackSuggested') ? Number(window.localStorage.getItem('coldPackSuggested')) : 0;
+    // console.log('coldPackSuggested is:');
+    // console.log(new Date(Number(coldPackSuggested)).toUTCString);
+    // console.log('hoursSince(coldPackSuggested) is:')
+    // console.log(hoursSince(coldPackSuggested));
 
 
 
-    // Sets black border around Shop link in navbar
-    document.getElementById('shopBtn').setAttribute('style', 'outline: 4px solid black; outline-offset:5px;');
 
-
+    // When page loads, if there is no cold pack in the cart, and it's been over an hour since the popup last appeared, then display a modal warning them
+    if (window.localStorage.getItem('PSecwid__2314096PScart').indexOf("COLDPACK") === -1 && hoursSince(coldPackSuggested) > 0.5) {
+        // No cold pack in the cart
+        $('#myModal').modal('show');
+        // Add an event listener for any click events
+        document.getElementById('myModal').addEventListener('click', modalClicked);
+        document.getElementById('rejectColdPack').addEventListener('click', rejectColdPack);
+    } else {
+        console.log('initial page load, there IS a cold pack');
+    }
+    
+    
     
     // If statement shows mobile navbar if device is smaller than 1080 pixels wide
     if (window.screen.width < 1080) {
@@ -19,6 +32,8 @@ $(document).ready(function() {
     // If statement shows desktop navbar if device is more than 1100 pixels wide
     if (window.screen.width >= 1080) {
         document.getElementById('desktopNav').setAttribute('style', 'display:block;');
+        // Sets black border around Shop link in navbar
+        document.getElementById('shopBtn').setAttribute('style', 'outline: 4px solid black; outline-offset:5px;');
     }
     
 
@@ -44,10 +59,6 @@ $(document).ready(function() {
 
 
 
-    window.addEventListener("hashchange", locationHashChanged);
-
-
-
     // Function that returns page content out of Firebase
     function getContent(pageName) {
         return firebase.database().ref('/' + pageName).once('value').then(function(pageContent) {
@@ -57,16 +68,31 @@ $(document).ready(function() {
 
 
 
-    function locationHashChanged() {
-        if(window.location.href.indexOf("cart") > -1) {
-            if (window.localStorage.getItem('PSecwid__2314096PScart').indexOf("COLDPACK") === -1) {
-                $('#coldPackWarning').slideDown(2000);
-                coldPackWarning = true;
-            }
-        } else if (coldPackWarning) {
-            $('#coldPackWarning').slideUp(2000);
-            coldPackWarning = false;
+    // Gets called when the cold pack warning modal receives a click event
+    function modalClicked(e) {
+        // Check whether the target of the click is the "Add to Bag" button
+        if (e.target.classList.contains('ecwid-AddToBagButton')) {
+            // If so, dismiss the modal, and set the time&date of the agreement.
+            $('#myModal').modal('hide');
+            window.localStorage.setItem('coldPackSuggested', Date.now());
         }
+    }
+
+
+
+    // Gets called when user clicks the "No thanks, I'll risk melted chocolates" button on the modal warning
+    function rejectColdPack() {
+        window.localStorage.setItem('coldPackSuggested', Date.now());
+    }
+
+
+
+    function hoursSince(date) {
+        // Difference between dates (in milliseconds) turned to regular seconds
+        var seconds = (new Date() - date) / 1000;
+        var minutes = seconds / 60;
+        var hours = Math.round(minutes / 60 * 100) / 100;
+        return hours;
     }
 
 
